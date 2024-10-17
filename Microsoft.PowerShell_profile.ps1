cfdcd3c2cee245deab5074c0e382523b7cf84afc
Michele Rossi <mikymod@gmail.com>
2024-10-17 15:33:31 +0200

    add git aliases for powershell

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,16 @@
+function gst { git status }
+function gaa { git add -A }
+function gd  { git diff }
+function gdc { git diff --cached }
+function gco { git checkout }
+function gfe { git fetch }
+function gre { git rebase }
+function glr { git l -30 }
+function glg { git lg }
+function com { git commit }
+function hs  { git rev-parse --short HEAD }
+function hm  { git log --format=%B -n 1 HEAD }
+function up  { git push }
+function upf { git push --force }
+function pu  { git pull }
+function pur { git pull --rebase }
--- install.ps1
+++ install.ps1
@@ -1,2 +1,5 @@
 cp .gitconfig $home
 cp .wezterm.lua $home
+
+mkdir -p $home/Documents/WindowsPowershell -ErrorAction SilentlyContinue
+cp Microsoft.PowerShell_profile.ps1 $home/Documents/WindowsPowershell
