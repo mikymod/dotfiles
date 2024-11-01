6566b705d10c3945ff8aa6e76677a1a8c65af178
Michele Rossi <mikymod@gmail.com>
2024-11-01 08:03:11 +0100

    set profile for powershell core

--- install.ps1
+++ install.ps1
@@ -3,3 +3,6 @@ cp .wezterm.lua $home
 
 mkdir -p $home/Documents/WindowsPowershell -ErrorAction SilentlyContinue
 cp Microsoft.PowerShell_profile.ps1 $home/Documents/WindowsPowershell
+
+mkdir -p $home/Documents/Powershell -ErrorAction SilentlyContinue
+cp Microsoft.PowerShell_profile.ps1 $home/Documents/Powershell
