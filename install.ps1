c4a3313b40de4f87c68622988d2e13388f86ab29
Michele Rossi <mikymod@gmail.com>
2025-07-30 09:04:45 +0200

    Update win install script

--- install.ps1
+++ install.ps1
@@ -1,8 +1,11 @@
-cp .gitconfig $home
-cp .wezterm.lua $home
+cp gitconfig $home/.gitconfig
+cp githelpers $home/.githelpers
+cp wezterm.lua $home/.wezterm.lua
+cp nvim_config.lua $env:LOCALAPPDATA/nvim
 
 mkdir -p $home/Documents/WindowsPowershell -ErrorAction SilentlyContinue
 cp Microsoft.PowerShell_profile.ps1 $home/Documents/WindowsPowershell
 
 mkdir -p $home/Documents/Powershell -ErrorAction SilentlyContinue
 cp Microsoft.PowerShell_profile.ps1 $home/Documents/Powershell
+
