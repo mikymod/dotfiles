2e19847cc125399fe39c31c295909ed4e7794355
Michele Rossi <mikymod@gmail.com>
2025-08-03 10:18:34 +0200

    Rename wezterm config

--- install.ps1
+++ install.ps1
@@ -1,7 +1,7 @@
 cp gitconfig $home/.gitconfig
 cp githelpers $home/.githelpers
 cp wezterm.lua $home/.wezterm.lua
-cp nvim_config.lua $env:LOCALAPPDATA/nvim
+cp nvim_config.lua $env:LOCALAPPDATA/nvim/init.lua
 
 mkdir -p $home/Documents/WindowsPowershell -ErrorAction SilentlyContinue
 cp Microsoft.PowerShell_profile.ps1 $home/Documents/WindowsPowershell
