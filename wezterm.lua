dbacc150447687dae5bc83cb3361a33fceab74a1
Michele Rossi <mikymod@gmail.com>
2025-08-03 13:20:15 +0200

    Improve wezterm bindings

--- wezterm.lua
+++ wezterm.lua
@@ -1,10 +1,8 @@
 local wezterm = require("wezterm")
 
-local mux = wezterm.mux
 local act = wezterm.action
 local config = {}
 local launch_menu = {}
-local haswork, work = pcall(require, "work")
 
 --- Setup PowerShell options
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
@@ -18,18 +16,14 @@ if wezterm.target_triple == "x86_64-pc-windows-msvc" then
 end
 
 local keys = {
-	{ key = "F11", mods = "NONE", action = act.ToggleFullScreen },
-	-- { key = "n", mods = "CTRL", action = act.SpawnWindow },
 
 	-- tab
-	{ key = "t", mods = "CTRL", action = act.SpawnTab("DefaultDomain") },
-	{ key = "w", mods = "SHIFT|CTRL", action = act.CloseCurrentTab({ confirm = true }) },
+	{ key = "t", mods = "LEADER", action = act.SpawnTab("DefaultDomain") },
+	{ key = "T", mods = "LEADER|SHIFT", action = act.CloseCurrentTab({ confirm = true }) },
 	{ key = "Tab", mods = "CTRL", action = act.ActivateTabRelative(1) },
 	{ key = "Tab", mods = "SHIFT|CTRL", action = act.ActivateTabRelative(-1) },
-	{ key = "PageUp", mods = "SHIFT", action = act.ScrollByPage(-1) },
 	{ key = "PageUp", mods = "CTRL", action = act.ActivateTabRelative(-1) },
 	{ key = "PageUp", mods = "SHIFT|CTRL", action = act.MoveTabRelative(-1) },
-	{ key = "PageDown", mods = "SHIFT", action = act.ScrollByPage(1) },
 	{ key = "PageDown", mods = "CTRL", action = act.ActivateTabRelative(1) },
 	{ key = "PageDown", mods = "SHIFT|CTRL", action = act.MoveTabRelative(1) },
 	{ key = "1", mods = "ALT", action = act.ActivateTab(0) },
@@ -50,6 +44,8 @@ local keys = {
 	{ key = "K", mods = "LEADER|SHIFT", action = act({ AdjustPaneSize = { "Up", 5 } }) },
 	{ key = "L", mods = "LEADER|SHIFT", action = act({ AdjustPaneSize = { "Right", 5 } }) },
 	{ key = "x", mods = "LEADER", action = act({ CloseCurrentPane = { confirm = true } }) },
+	{ key = "o", mods = "LEADER", action = act.TogglePaneZoomState },
+	{ key = "z", mods = "LEADER", action = act.TogglePaneZoomState },
 
 	-- font size
 	{ key = "+", mods = "SHIFT|CTRL", action = act.IncreaseFontSize },
@@ -87,22 +83,23 @@ local keys = {
 	{ key = "f", mods = "SHIFT|CTRL", action = act.Search("CurrentSelectionOrEmptyString") },
 
 	-- misc
+	{ key = "F11", mods = "NONE", action = act.ToggleFullScreen },
+	{ key = "n", mods = "LEADER", action = act.SpawnWindow },
 	{ key = "r", mods = "LEADER", action = act.ReloadConfiguration },
+	{ key = "R", mods = "LEADER", action = act.ReloadConfiguration },
+
 	{ key = "L", mods = "CTRL", action = act.ShowDebugOverlay },
 	{ key = "L", mods = "SHIFT|CTRL", action = act.ShowDebugOverlay },
 	{ key = "l", mods = "SHIFT|CTRL", action = act.ShowDebugOverlay },
+
 	{ key = "phys:Space", mods = "SHIFT|CTRL", action = act.QuickSelect },
 	{ key = "x", mods = "SHIFT|CTRL", action = act.ActivateCopyMode },
-	{ key = "z", mods = "SHIFT|CTRL", action = act.TogglePaneZoomState },
 	{ key = "P", mods = "CTRL", action = act.ActivateCommandPalette },
-	{ key = "R", mods = "CTRL", action = act.ReloadConfiguration },
-	{ key = "R", mods = "SHIFT|CTRL", action = act.ReloadConfiguration },
 	{ key = "t", mods = "SHIFT|CTRL", action = act.ShowLauncher },
 	{ key = "T", mods = "CTRL", action = act.ShowLauncher },
 	{ key = "T", mods = "SHIFT|CTRL", action = act.ShowLauncher },
 	{ key = "X", mods = "CTRL", action = act.ActivateCopyMode },
 	{ key = "X", mods = "SHIFT|CTRL", action = act.ActivateCopyMode },
-	{ key = "Z", mods = "CTRL", action = act.TogglePaneZoomState },
 	{ key = "Z", mods = "SHIFT|CTRL", action = act.TogglePaneZoomState },
 	{ key = "m", mods = "SHIFT|CTRL", action = act.Hide },
 	{ key = "M", mods = "CTRL", action = act.Hide },
@@ -150,9 +147,9 @@ local colors = {
 	split = "#444444",
 
 	ansi = {
-		"#1d2021", -- black, color 0
-		"#cc241d", -- red, color 1
-		"#98971a", -- green, color 2
+		"#1d2021",
+		"#cc241d",
+		"#98971a",
 		"#d79921",
 		"#458588",
 		"#b16286",
@@ -160,9 +157,9 @@ local colors = {
 		"#a89984",
 	},
 	brights = {
-		"#7c6f64", -- black, color 0
-		"#fb4934", -- red, color 1
-		"#b8bb26", -- green, color 2
+		"#7c6f64",
+		"#fb4934",
+		"#b8bb26",
 		"#fabd2f",
 		"#83a598",
 		"#d3869b",
@@ -175,7 +172,8 @@ local colors = {
 config.scrollback_lines = 10000
 config.hyperlink_rules = wezterm.default_hyperlink_rules()
 config.hide_tab_bar_if_only_one_tab = true
-config.color_scheme = "GruvboxDarkHard"
+--config.color_scheme = "Gruvbox Dark (Gogh)"
+--config.color_scheme = "GruvboxDarkHard"
 config.font = wezterm.font("FiraCode Nerd Font")
 
 config.font_size = 11
@@ -186,6 +184,6 @@ config.disable_default_key_bindings = true
 config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
 config.keys = keys
 config.mouse_bindings = mouse_bindings
---config.colors = colors
+config.colors = colors
 
 return config
