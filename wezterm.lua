9734085a72f9f6738b5bcddaf832b09c84748f48
Michele Rossi <mikymod@gmail.com>
2025-12-18 18:23:40 +0100

    Add msys conf to wezterm

--- wezterm.lua
+++ wezterm.lua
@@ -6,166 +6,170 @@ local launch_menu = {}
 
 --- Setup PowerShell options
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-	--- Grab the ver info for later use.
-	-- local success, stdout, stderr = wezterm.run_child_process({ "cmd.exe", "ver" })
-	-- local major, minor, build, rev = stdout:match("Version ([0-9]+)%.([0-9]+)%.([0-9]+)%.([0-9]+)")
-	-- local is_windows_11 = tonumber(build) >= 22000
+    --- Grab the ver info for later use.
+    -- local success, stdout, stderr = wezterm.run_child_process({ "cmd.exe", "ver" })
+    -- local major, minor, build, rev = stdout:match("Version ([0-9]+)%.([0-9]+)%.([0-9]+)%.([0-9]+)")
+    -- local is_windows_11 = tonumber(build) >= 22000
+    table.insert(launch_menu, {
+        label = "MSYS UCRT64",
+        args = { "cmd.exe ", "/k", "C:\\msys64\\msys2_shell.cmd -defterm -here -no-start -ucrt64 -shell bash" },
+    });
 
-	--- Set Pwsh as the default on Windows
-	config.default_prog = { "pwsh.exe", "-NoLogo" }
+    --- Set Pwsh as the default on Windows
+    config.default_prog = { "pwsh.exe", "-NoLogo" }
 end
 
 local keys = {
 
-	-- tab
-	{ key = "t", mods = "LEADER", action = act.SpawnTab("DefaultDomain") },
-	{ key = "T", mods = "LEADER|SHIFT", action = act.CloseCurrentTab({ confirm = true }) },
-	{ key = "Tab", mods = "CTRL", action = act.ActivateTabRelative(1) },
-	{ key = "Tab", mods = "SHIFT|CTRL", action = act.ActivateTabRelative(-1) },
-	{ key = "PageUp", mods = "CTRL", action = act.ActivateTabRelative(-1) },
-	{ key = "PageUp", mods = "SHIFT|CTRL", action = act.MoveTabRelative(-1) },
-	{ key = "PageDown", mods = "CTRL", action = act.ActivateTabRelative(1) },
-	{ key = "PageDown", mods = "SHIFT|CTRL", action = act.MoveTabRelative(1) },
-	{ key = "1", mods = "ALT", action = act.ActivateTab(0) },
-	{ key = "2", mods = "ALT", action = act.ActivateTab(1) },
-	{ key = "3", mods = "ALT", action = act.ActivateTab(2) },
-	{ key = "4", mods = "ALT", action = act.ActivateTab(3) },
-	{ key = "5", mods = "ALT", action = act.ActivateTab(4) },
+    -- tab
+    { key = "t",        mods = "LEADER",       action = act.SpawnTab("DefaultDomain") },
+    { key = "T",        mods = "LEADER|SHIFT", action = act.CloseCurrentTab({ confirm = true }) },
+    { key = "Tab",      mods = "CTRL",         action = act.ActivateTabRelative(1) },
+    { key = "Tab",      mods = "SHIFT|CTRL",   action = act.ActivateTabRelative(-1) },
+    { key = "PageUp",   mods = "CTRL",         action = act.ActivateTabRelative(-1) },
+    { key = "PageUp",   mods = "SHIFT|CTRL",   action = act.MoveTabRelative(-1) },
+    { key = "PageDown", mods = "CTRL",         action = act.ActivateTabRelative(1) },
+    { key = "PageDown", mods = "SHIFT|CTRL",   action = act.MoveTabRelative(1) },
+    { key = "1",        mods = "ALT",          action = act.ActivateTab(0) },
+    { key = "2",        mods = "ALT",          action = act.ActivateTab(1) },
+    { key = "3",        mods = "ALT",          action = act.ActivateTab(2) },
+    { key = "4",        mods = "ALT",          action = act.ActivateTab(3) },
+    { key = "5",        mods = "ALT",          action = act.ActivateTab(4) },
 
-	-- panel
-	{ key = "|", mods = "LEADER|SHIFT", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
-	{ key = "_", mods = "LEADER|SHIFT", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
-	{ key = "h", mods = "LEADER", action = act({ ActivatePaneDirection = "Left" }) },
-	{ key = "j", mods = "LEADER", action = act({ ActivatePaneDirection = "Down" }) },
-	{ key = "k", mods = "LEADER", action = act({ ActivatePaneDirection = "Up" }) },
-	{ key = "l", mods = "LEADER", action = act({ ActivatePaneDirection = "Right" }) },
-	{ key = "H", mods = "LEADER|SHIFT", action = act({ AdjustPaneSize = { "Left", 5 } }) },
-	{ key = "J", mods = "LEADER|SHIFT", action = act({ AdjustPaneSize = { "Down", 5 } }) },
-	{ key = "K", mods = "LEADER|SHIFT", action = act({ AdjustPaneSize = { "Up", 5 } }) },
-	{ key = "L", mods = "LEADER|SHIFT", action = act({ AdjustPaneSize = { "Right", 5 } }) },
-	{ key = "x", mods = "LEADER", action = act({ CloseCurrentPane = { confirm = true } }) },
-	{ key = "o", mods = "LEADER", action = act.TogglePaneZoomState },
-	{ key = "z", mods = "LEADER", action = act.TogglePaneZoomState },
+    -- panel
+    { key = "|",        mods = "LEADER|SHIFT", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+    { key = "_",        mods = "LEADER|SHIFT", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+    { key = "h",        mods = "LEADER",       action = act({ ActivatePaneDirection = "Left" }) },
+    { key = "j",        mods = "LEADER",       action = act({ ActivatePaneDirection = "Down" }) },
+    { key = "k",        mods = "LEADER",       action = act({ ActivatePaneDirection = "Up" }) },
+    { key = "l",        mods = "LEADER",       action = act({ ActivatePaneDirection = "Right" }) },
+    { key = "H",        mods = "LEADER|SHIFT", action = act({ AdjustPaneSize = { "Left", 5 } }) },
+    { key = "J",        mods = "LEADER|SHIFT", action = act({ AdjustPaneSize = { "Down", 5 } }) },
+    { key = "K",        mods = "LEADER|SHIFT", action = act({ AdjustPaneSize = { "Up", 5 } }) },
+    { key = "L",        mods = "LEADER|SHIFT", action = act({ AdjustPaneSize = { "Right", 5 } }) },
+    { key = "x",        mods = "LEADER",       action = act({ CloseCurrentPane = { confirm = true } }) },
+    { key = "o",        mods = "LEADER",       action = act.TogglePaneZoomState },
+    { key = "z",        mods = "LEADER",       action = act.TogglePaneZoomState },
 
-	-- font size
-	{ key = "+", mods = "SHIFT|CTRL", action = act.IncreaseFontSize },
-	{ key = "=", mods = "CTRL", action = act.IncreaseFontSize },
-	{ key = "-", mods = "CTRL", action = act.DecreaseFontSize },
-	{ key = "0", mods = "CTRL", action = act.ResetFontSize },
+    -- font size
+    { key = "+",        mods = "SHIFT|CTRL",   action = act.IncreaseFontSize },
+    { key = "=",        mods = "CTRL",         action = act.IncreaseFontSize },
+    { key = "-",        mods = "CTRL",         action = act.DecreaseFontSize },
+    { key = "0",        mods = "CTRL",         action = act.ResetFontSize },
 
-	-- copy paste
-	{ key = "C", mods = "CTRL", action = act.CopyTo("Clipboard") },
-	{ key = "C", mods = "SHIFT|CTRL", action = act.CopyTo("Clipboard") },
-	{ key = "c", mods = "SHIFT|CTRL", action = act.CopyTo("Clipboard") },
-	{ key = "V", mods = "CTRL", action = act.PasteFrom("Clipboard") },
-	-- { key = "V",        mods = "SHIFT|CTRL",     action = act.PasteFrom("Clipboard") },
-	{ key = "Insert", mods = "SHIFT", action = act.PasteFrom("PrimarySelection") },
-	{ key = "Insert", mods = "CTRL", action = act.CopyTo("PrimarySelection") },
-	{
-		key = "u",
-		mods = "SHIFT|CTRL",
-		action = act.CharSelect({ copy_on_select = true, copy_to = "ClipboardAndPrimarySelection" }),
-	},
-	{
-		key = "U",
-		mods = "CTRL",
-		action = act.CharSelect({ copy_on_select = true, copy_to = "ClipboardAndPrimarySelection" }),
-	},
-	{
-		key = "U",
-		mods = "SHIFT|CTRL",
-		action = act.CharSelect({ copy_on_select = true, copy_to = "ClipboardAndPrimarySelection" }),
-	},
+    -- copy paste
+    { key = "C",        mods = "CTRL",         action = act.CopyTo("Clipboard") },
+    { key = "C",        mods = "SHIFT|CTRL",   action = act.CopyTo("Clipboard") },
+    { key = "c",        mods = "SHIFT|CTRL",   action = act.CopyTo("Clipboard") },
+    { key = "V",        mods = "CTRL",         action = act.PasteFrom("Clipboard") },
+    -- { key = "V",        mods = "SHIFT|CTRL",     action = act.PasteFrom("Clipboard") },
+    { key = "Insert",   mods = "SHIFT",        action = act.PasteFrom("PrimarySelection") },
+    { key = "Insert",   mods = "CTRL",         action = act.CopyTo("PrimarySelection") },
+    {
+        key = "u",
+        mods = "SHIFT|CTRL",
+        action = act.CharSelect({ copy_on_select = true, copy_to = "ClipboardAndPrimarySelection" }),
+    },
+    {
+        key = "U",
+        mods = "CTRL",
+        action = act.CharSelect({ copy_on_select = true, copy_to = "ClipboardAndPrimarySelection" }),
+    },
+    {
+        key = "U",
+        mods = "SHIFT|CTRL",
+        action = act.CharSelect({ copy_on_select = true, copy_to = "ClipboardAndPrimarySelection" }),
+    },
 
-	-- Search
-	{ key = "f", mods = "CTRL", action = act.Search("CurrentSelectionOrEmptyString") },
-	{ key = "F", mods = "SHIFT|CTRL", action = act.Search("CurrentSelectionOrEmptyString") },
-	{ key = "f", mods = "SHIFT|CTRL", action = act.Search("CurrentSelectionOrEmptyString") },
+    -- Search
+    { key = "f",          mods = "CTRL",       action = act.Search("CurrentSelectionOrEmptyString") },
+    { key = "F",          mods = "SHIFT|CTRL", action = act.Search("CurrentSelectionOrEmptyString") },
+    { key = "f",          mods = "SHIFT|CTRL", action = act.Search("CurrentSelectionOrEmptyString") },
 
-	-- misc
-	{ key = "F11", mods = "NONE", action = act.ToggleFullScreen },
-	{ key = "n", mods = "LEADER", action = act.SpawnWindow },
-	{ key = "r", mods = "LEADER", action = act.ReloadConfiguration },
-	{ key = "R", mods = "LEADER", action = act.ReloadConfiguration },
+    -- misc
+    { key = "F11",        mods = "NONE",       action = act.ToggleFullScreen },
+    { key = "n",          mods = "LEADER",     action = act.SpawnWindow },
+    { key = "r",          mods = "LEADER",     action = act.ReloadConfiguration },
+    { key = "R",          mods = "LEADER",     action = act.ReloadConfiguration },
 
-	{ key = "L", mods = "CTRL", action = act.ShowDebugOverlay },
-	{ key = "L", mods = "SHIFT|CTRL", action = act.ShowDebugOverlay },
-	{ key = "l", mods = "SHIFT|CTRL", action = act.ShowDebugOverlay },
+    { key = "L",          mods = "CTRL",       action = act.ShowDebugOverlay },
+    { key = "L",          mods = "SHIFT|CTRL", action = act.ShowDebugOverlay },
+    { key = "l",          mods = "SHIFT|CTRL", action = act.ShowDebugOverlay },
 
-	{ key = "phys:Space", mods = "SHIFT|CTRL", action = act.QuickSelect },
-	{ key = "x", mods = "SHIFT|CTRL", action = act.ActivateCopyMode },
-	{ key = "P", mods = "CTRL", action = act.ActivateCommandPalette },
-	{ key = "t", mods = "SHIFT|CTRL", action = act.ShowLauncher },
-	{ key = "T", mods = "CTRL", action = act.ShowLauncher },
-	{ key = "T", mods = "SHIFT|CTRL", action = act.ShowLauncher },
-	{ key = "X", mods = "CTRL", action = act.ActivateCopyMode },
-	{ key = "X", mods = "SHIFT|CTRL", action = act.ActivateCopyMode },
-	{ key = "Z", mods = "SHIFT|CTRL", action = act.TogglePaneZoomState },
-	{ key = "m", mods = "SHIFT|CTRL", action = act.Hide },
-	{ key = "M", mods = "CTRL", action = act.Hide },
-	{ key = "M", mods = "SHIFT|CTRL", action = act.Hide },
-	{ key = "k", mods = "SHIFT|CTRL", action = act.ClearScrollback("ScrollbackOnly") },
+    { key = "phys:Space", mods = "SHIFT|CTRL", action = act.QuickSelect },
+    { key = "x",          mods = "SHIFT|CTRL", action = act.ActivateCopyMode },
+    { key = "P",          mods = "CTRL",       action = act.ActivateCommandPalette },
+    { key = "t",          mods = "SHIFT|CTRL", action = act.ShowLauncher },
+    { key = "T",          mods = "CTRL",       action = act.ShowLauncher },
+    { key = "T",          mods = "SHIFT|CTRL", action = act.ShowLauncher },
+    { key = "X",          mods = "CTRL",       action = act.ActivateCopyMode },
+    { key = "X",          mods = "SHIFT|CTRL", action = act.ActivateCopyMode },
+    { key = "Z",          mods = "SHIFT|CTRL", action = act.TogglePaneZoomState },
+    { key = "m",          mods = "SHIFT|CTRL", action = act.Hide },
+    { key = "M",          mods = "CTRL",       action = act.Hide },
+    { key = "M",          mods = "SHIFT|CTRL", action = act.Hide },
+    { key = "k",          mods = "SHIFT|CTRL", action = act.ClearScrollback("ScrollbackOnly") },
 }
 
 -- Mousing bindings
 local mouse_bindings = {
-	-- Change the default click behavior so that it only selects
-	-- text and doesn't open hyperlinks
-	{
-		event = { Up = { streak = 1, button = "Left" } },
-		mods = "NONE",
-		action = act.CompleteSelection("ClipboardAndPrimarySelection"),
-	},
+    -- Change the default click behavior so that it only selects
+    -- text and doesn't open hyperlinks
+    {
+        event = { Up = { streak = 1, button = "Left" } },
+        mods = "NONE",
+        action = act.CompleteSelection("ClipboardAndPrimarySelection"),
+    },
 
-	-- and make CTRL-Click open hyperlinks
-	{
-		event = { Up = { streak = 1, button = "Left" } },
-		mods = "CTRL",
-		action = act.OpenLinkAtMouseCursor,
-	},
-	{
-		event = { Down = { streak = 3, button = "Left" } },
-		action = wezterm.action.SelectTextAtMouseCursor("SemanticZone"),
-		mods = "NONE",
-	},
+    -- and make CTRL-Click open hyperlinks
+    {
+        event = { Up = { streak = 1, button = "Left" } },
+        mods = "CTRL",
+        action = act.OpenLinkAtMouseCursor,
+    },
+    {
+        event = { Down = { streak = 3, button = "Left" } },
+        action = wezterm.action.SelectTextAtMouseCursor("SemanticZone"),
+        mods = "NONE",
+    },
 }
 
 local colors = {
-	foreground = "#fbf1c7",
-	background = "#1d2021",
+    foreground = "#fbf1c7",
+    background = "#1d2021",
 
-	cursor_bg = "#928374",
-	cursor_fg = "black",
-	cursor_border = "#928374",
+    cursor_bg = "#928374",
+    cursor_fg = "black",
+    cursor_border = "#928374",
 
-	selection_fg = "#928374",
-	selection_bg = "#ebdbb2",
+    selection_fg = "#928374",
+    selection_bg = "#ebdbb2",
 
-	scrollbar_thumb = "#222222",
+    scrollbar_thumb = "#222222",
 
-	-- The color of the split lines between panes
-	split = "#444444",
+    -- The color of the split lines between panes
+    split = "#444444",
 
-	ansi = {
-		"#1d2021",
-		"#cc241d",
-		"#98971a",
-		"#d79921",
-		"#458588",
-		"#b16286",
-		"#689d6a",
-		"#a89984",
-	},
-	brights = {
-		"#7c6f64",
-		"#fb4934",
-		"#b8bb26",
-		"#fabd2f",
-		"#83a598",
-		"#d3869b",
-		"#8ec07c",
-		"#fbf1c7",
-	},
+    ansi = {
+        "#1d2021",
+        "#cc241d",
+        "#98971a",
+        "#d79921",
+        "#458588",
+        "#b16286",
+        "#689d6a",
+        "#a89984",
+    },
+    brights = {
+        "#7c6f64",
+        "#fb4934",
+        "#b8bb26",
+        "#fabd2f",
+        "#83a598",
+        "#d3869b",
+        "#8ec07c",
+        "#fbf1c7",
+    },
 }
 
 --- Default config settings
