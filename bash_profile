418b287288519bcd81c76fa4262e16386fd0013b
Michele Rossi <mikymod@gmail.com>
2026-09-25 18:27:20 +0200

    Update dotfiles

--- bash_profile
+++ bash_profile
@@ -3,3 +3,7 @@ source .bashrc
 if [ -z "$WAYLAND_DISPLAY" ] && [ "$(tty)" = "/dev/tty1" ]; then
     start-hyprland
 fi
+
+# bun
+export BUN_INSTALL="$HOME/.bun"
+export PATH="$BUN_INSTALL/bin:$PATH"
--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -5,6 +5,7 @@ return {
         require("nvim-tree").setup({
             view = { width = 30 },
             renderer = { group_empty = true },
+            filters = { git_ignored = false },
         })
     end,
 }
